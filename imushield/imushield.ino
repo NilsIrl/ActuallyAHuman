72fd036d9ef4b50701dfb264356e60d596d38cd9
Nils <nils@nilsand.re>
2025-02-16 01:37:13 -0800

    simplify imu code

--- imushield/imushield.ino
+++ imushield/imushield.ino
@@ -2,10 +2,9 @@
 #include <Wire.h>
 
 NineAxesMotion mySensor;
-unsigned long lastStreamTime = 0;
-const int streamPeriod = 20;  
 
 float prevHeading = 0.0;
+// heading that doesn't wrap around 360.0
 float continuousHeading = 0.0;
 
 void setup() 
@@ -20,26 +19,22 @@ void setup()
 
 void loop() 
 {
-  if ((millis() - lastStreamTime) >= streamPeriod)
+  mySensor.updateEuler();
+
+  float heading = mySensor.readEulerHeading();
+
+  float delta = heading - prevHeading;
+  if (delta > 180.0) 
   {
-    lastStreamTime = millis();
-    mySensor.updateEuler();
-
-    float heading = mySensor.readEulerHeading();
-
-    float delta = heading - prevHeading;
-    if (delta > 180.0) 
-    {
-      continuousHeading -= 360.0;
-    } 
-    else if (delta < -180.0) 
-    {
-      continuousHeading += 360.0;
-    }
-
-    continuousHeading += delta;
-    prevHeading = heading;
-
-    Serial.println(continuousHeading);  // Output only the heading
+    continuousHeading -= 360.0;
+  } 
+  else if (delta < -180.0) 
+  {
+    continuousHeading += 360.0;
   }
+
+  continuousHeading += delta;
+  prevHeading = heading;
+
+  Serial.println(continuousHeading);
 }
